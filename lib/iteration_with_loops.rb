e81b81d6e56db3d5203a609b0bbfa757bdcb1b7b
Chaim Bryski <luzybry94@gmail.com>
2020-05-13 00:24:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
     str = ""
     inner_index = 0
     while inner_index < src[outer_index].length do
-      if src[outer_index][inner_index].class == string
+      if src[outer_index][inner_index].class == String
         str += src[outer_index][inner_index] + " "
       end
       inner_index += 1
